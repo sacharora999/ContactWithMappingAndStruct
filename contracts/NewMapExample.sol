8ebb69dd7cd5f365a3a1aedee961e36edef3920d
sacharora999 <sacharora999@gmail.com>
2023-06-08 06:32:28 +0530

    Ethereum Wallet With Mapping

---
+++ contracts/NewMapExample.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract NewMapExample
+{
+    mapping(address => uint) public balanceReceived;
+
+    function depositMoney() public payable
+    {
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function getAccBal() public view returns(uint){
+        return balanceReceived[msg.sender];
+    }
+
+    function withDrawMonaey(address payable _to, uint _amount) public {
+        require(balanceReceived[msg.sender] >= _amount, "Not Sufficient Balance");
+        balanceReceived[msg.sender]  -= _amount;
+        _to.transfer(_amount);
+    }
+}
